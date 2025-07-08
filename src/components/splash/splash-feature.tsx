0c41bb3398dc711dc9783b83f552f26afed30023
pranjal_29092005 <pranjalkgupta555@gmail.com>
2025-07-08 15:58:10 +0530

    Add files via upload

---
+++ src/components/splash/splash-feature.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletButton } from '../solana/solana-provider'
+import { AppHero, ellipsify } from '../ui/ui-layout'
+import { ExplorerLink } from '../cluster/cluster-ui'
+import { useSplashProgram } from './splash-data-access'
+
+
+export default function SplashFeature() {
+  const { publicKey } = useWallet()
+  const { programId } = useSplashProgram()
+
+  return publicKey ? (
+    <div>
+      <AppHero
+        title="Splash"
+        subtitle={
+          'Create a new account by clicking the "Create" button. The state of a account is stored on-chain and can be manipulated by calling the program\'s methods (increment, decrement, set, and close).'
+        }
+      >
+        <p className="mb-6">
+          <ExplorerLink path={`account/${programId}`} label={ellipsify(programId.toString())} />
+        </p>
+      </AppHero>
+
+    </div>
+  ) : (
+    <div className="max-w-4xl mx-auto">
+      <div className="hero py-[64px]">
+        <div className="hero-content text-center">
+          <WalletButton />
+        </div>
+      </div>
+    </div>
+  )
+}
