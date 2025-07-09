bd1c0fb462b4bb042620526c54de2db3c81cc540
Anand Raj Tripathi <htripathi020@gmail.com>
2025-07-09 13:35:43 +0530

    Add files via upload

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,45 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Uploader } from "@irys/upload";
+import { Solana } from "@irys/upload-solana";
 
+const getIrysUploader = async () => {
+  const irysUploader = await Uploader(Solana).withWallet(
+    process.env.PRIVATE_KEY
+  );
+  return irysUploader;
+};
 
-export async function GET() {
-
-  return Response.json({message: "Hello World"});
-}
-// import NextAuth, { NextAuthOptions } from "next-auth";
-  // import GoogleProvider from "next-auth/providers/google";
-  // declare module "next-auth" {
-  //   interface Session {
-  //     accessToken?: string;
-  //   }
+export async function POST(req: NextRequest) {
+  const irysUploader = await getIrysUploader();
   
-  //   interface JWT {
-  //     accessToken?: string;
-  //   }
-  // }
-  // // Define the authentication options
-  // export const authOptions: NextAuthOptions = {
-  //   providers: [
-  //     GoogleProvider({
-  //       clientId: process.env.GOOGLE_CLIENT_ID as string,
-  //       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-  //     }),
-  //   ],
-  //   callbacks: {
-  //     async session({ session, token }) {
-  //       // Explicitly type session and token
-  //       session.accessToken = token.accessToken as string | undefined;
-  //       console.log(session);
-  //       return session;
-  //     },
-  //     async jwt({ token, account }) {
-  //       // Persist the access_token in the token object
-  //       if (account) {
-  //         token.accessToken = account.access_token as string | undefined;
-  //       }
-  //       return token;
-  //     },
-  //   },
-  // };
+  try {
+    const data  = await req.json(); 
+    console.log("Received data: ", data);  // Debug: log the received data
 
-  // // Export the NextAuth handler
-  // const handler = NextAuth(authOptions);
-  // export { handler as GET, handler as POST };
+    if (!data) {
+      throw new Error("No data provided in request body");
+    }
+
+    const bufferData = Buffer.from(JSON.stringify(data), 'utf-8');
+    const tags = [{ name: "Content-Type", value: "application/json" }];
+    const receipt = await irysUploader.upload(bufferData, { tags }); 
+    
+    console.log(`Data uploaded ==> https://gateway.irys.xyz/${receipt.id}`);
+    
+    return NextResponse.json({ txurl: `https://gateway.irys.xyz/${receipt.id}`, txid: receipt.id });
+  } catch (e) {
+    console.error("Error when uploading ", e);
+
+    return NextResponse.json({ error: "Upload failed" }, { status: 500 });
+  }
+}
